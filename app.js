591eb898bd544e8942f5b5a3bfbd58543d6b5f5e
Tamir Montalto <tmontalto@MacBook-Pro-2.fios-router.home>
2021-08-09 12:24:38 -0400

    list items render

---
+++ app.js
@@ -0,0 +1,14 @@
+const submitButton = document.querySelector('#submit')
+const toDoList = document.querySelector('.to-do-list')
+const itemsCompleted = document.querySelector('.completed-list')
+
+const listArray = []
+
+const renderList = () => {
+    let listItem = document.createElement('li')
+
+    for (let items of listArray) {
+        listItem.innerText = items;
+        toDoList.appendChild(listItem)
+    }
+}
