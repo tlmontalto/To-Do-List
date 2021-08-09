2b8937cc0a0c7f1f438e3caefd0b2c5ecfb0a6e3
Tamir Montalto <tmontalto@MacBook-Pro-2.fios-router.home>
2021-08-09 18:36:55 -0400

    reverted back to previous method, still need to work out remove button

--- app.js
+++ app.js
@@ -2,33 +2,34 @@ const submitButton = document.querySelector('#submit')
 const toDoList = document.querySelector('.to-do-list')
 const itemsCompleted = document.querySelector('.completed-list')
 
-const removeBtn = document.createElement('button')
-const completeBtn = document.createElement('button');
-let listItem = document.createElement('li')
-
 const listArray = []
 
 const renderList = () => {
-
-    completeBtn.innerText = 'Completed'
+    let listItem = document.createElement('li')
 
     for (let items of listArray) {
         listItem.innerText = items
         toDoList.appendChild(listItem)
-        listItem.append(completeBtn)
     }
+
+    const completeBtn = document.createElement('button');
+    completeBtn.innerText = 'Completed'
+    listItem.append(completeBtn)
+    listItem.append(completeBtn)
 
     completeBtn.onclick = () => {
         listItem.remove()
         completeBtn.remove()
         itemsCompleted.append(listItem)
+    }
 
-        removeBtn.innerText = 'Remove'
-        listItem.appendChild(removeBtn)
-        removeBtn.onclick = () => {
-            listItem.remove()
-            removeBtn.remove()
-        }
+    const removeBtn = document.createElement('button')
+    removeBtn.innerText = 'Remove'
+    listItem.appendChild(removeBtn)
+
+    removeBtn.onclick = () => {
+        listItem.remove()
+        removeBtn.remove()
     }
 }
 
